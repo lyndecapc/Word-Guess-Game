2c1e01e05b8a7db3cf74ad6b31b2a2e652aaa8e7
Lynde Capogreco <lyndecapc@gmail.com>
2020-01-05 15:42:41 -0600

    added spaces for secret word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //possible words for hangman game//
-var wordList = ["Sheldon", "Leonard", "Rajesh", "Howard", "Penny", "Bernadette", "physics", "theoretical", "bazinga", "Pasadena", "comic books", "Spock", "brisket", "nerds", "eidetic"];
+var wordList = ["SHELDON", "LEONARD", "RAJ", "HOWARD", "PENNY", "BERNADETTE", "PHYSICS", "THEORETICAL", "BAZINGA", "TRAINS", "COMICS", "SPOCK", "BRISKET", "NERDS", "EIDETIC"];
 
 //letters for player to choose//
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -9,19 +9,31 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var randomWord = wordList[Math.floor(Math.random()*wordList.length)];
 
 //check to make sure random word works
-console.log("Random Word: " + randomWord);
+console.log("Secret Word: " + randomWord);
 
-var attempts = 10;
+    var directionsText = document.getElementById("directions-text");
+    var secretWordText = document.getElementById("secretword-text");
+    var remainingguessesText = document.getElementById("remainingguesses-text");
+    var lettersguessedText = document.getElementById("lettersguessed-text");
+    var winsText = document.getElementById("wins-text");
+    
+
+var remainingGuesses = 10;
 var wins = 0;
 var lettersGuessed = [];
 var secretWord = [];
 
 //spaces for secret word letters//
-for (var i = 0; i < randomWord.length; i++) {
-    secretWord.push("_");   
-}
  
+for (var i = 0; i < randomWord.length; i++) {
+    secretWord[i] = "-";
+}
+secretWordText.textContent = secretWord.join("");
+
+for (var i = 0; i < randomWord.length; i++) {
+    secretWord[i] = randomWord[i];
+}
 
 document.onkeyup = function(event) {
-    var lettersGuessed = event.key;
+     letters = event.key.toUpperCase(); 
 }
